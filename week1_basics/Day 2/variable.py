43b27954bc0c664af2fa24bd64bb11d4ada45ff9
Ayomikun Esan <esanemmanuel013@gmail.com>
2025-12-15 16:04:02 +0100

    Add initial greeting and robot status scripts for Week 1

---
+++ week1_basics/Day 2/variable.py
@@ -0,0 +1,10 @@
+robot_name = "AyoBot"
+speed = 50
+battery_percentage = 85.5
+is_active = True
+
+print("n\-- Robot Active Status --")
+print(f"Robot Name: {robot_name}")
+print(f"Speed: {speed} units")
+print(f"Battery Percentage: {battery_percentage}%")
+print(f"Is Active: {is_active}")
